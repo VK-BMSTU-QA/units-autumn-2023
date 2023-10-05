4a8a7dcfc697863446ef011d8b81807fd392a6e0
Kirill Kiselev <lirikk285@mail.ru>
2023-10-05 20:50:03 +0300

    added frontend unit test

---
+++ frontend/src/utils/__tests__/applyCategories.test.ts
@@ -0,0 +1,63 @@
+import { Product } from '../../types';
+import { applyCategories } from '../applyCategories';
+
+describe('test apply categories function', () => {
+    it('return all products', () => {
+        let products: Product[] = [
+            {
+                id: 1,
+                name: 'Iphone',
+                description: '',
+                category: 'Электроника',
+                price: 10,
+            },
+            {
+                id: 2,
+                name: 'Cap',
+                description: '',
+                category: 'Одежда',
+                price: 5,
+            },
+        ];
+
+        expect(applyCategories(products, [])).toStrictEqual(products);
+    });
+
+    it('should filter products', () => {
+        let products: Product[] = [
+            {
+                id: 1,
+                name: 'Iphone',
+                description: '',
+                category: 'Электроника',
+                price: 10,
+            },
+            {
+                id: 2,
+                name: 'Cap',
+                description: '',
+                category: 'Одежда',
+                price: 5,
+            },
+            {
+                id: 3,
+                name: 'Pants',
+                description: '',
+                category: 'Одежда',
+                price: 5,
+            },
+        ];
+
+        let expected: Product[] = [
+            {
+                id: 1,
+                name: 'Iphone',
+                description: '',
+                category: 'Электроника',
+                price: 10,
+            },
+        ];
+
+		expect(applyCategories(products, ['Электроника'])).toStrictEqual(expected)
+    });
+});
