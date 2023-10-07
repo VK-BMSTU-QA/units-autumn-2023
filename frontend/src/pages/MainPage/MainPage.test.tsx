6970cb7107f4222fb5ab27f811fc73bfb5eb2a51
Kirill Kiselev <lirikk285@mail.ru>
2023-10-07 14:48:19 +0300

    added date mock

--- frontend/src/pages/MainPage/MainPage.test.tsx
+++ frontend/src/pages/MainPage/MainPage.test.tsx
@@ -4,9 +4,15 @@ import '@testing-library/jest-dom';
 import { MainPage } from './MainPage';
 
 afterEach(jest.clearAllMocks);
+
 describe('Main page test', () => {
+    jest.spyOn(Date.prototype, 'toLocaleTimeString').mockReturnValue(
+        '14:00:00'
+    );
+	
     it('should render correctly', () => {
         const rendered = render(<MainPage />);
 
+        expect(rendered.asFragment()).toMatchSnapshot();
     });
 });
