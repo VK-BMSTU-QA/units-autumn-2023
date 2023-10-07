f8b056fff5e6e527a86e1fa655d52da5b9fa9893
Kirill Kiselev <lirikk285@mail.ru>
2023-10-07 18:12:33 +0300

    added mainpage tests

--- frontend/src/pages/MainPage/MainPage.test.tsx
+++ frontend/src/pages/MainPage/MainPage.test.tsx
@@ -3,16 +3,66 @@ import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { MainPage } from './MainPage';
 
+import { applyCategories } from '../../utils';
+
 afterEach(jest.clearAllMocks);
 
+jest.mock('../../utils', () => {
+    const original = jest.requireActual('../../utils');
+    const products = [
+        {
+            id: 1,
+            name: 'Iphone',
+            description: '',
+            category: 'Электроника',
+            price: 10,
+        },
+        {
+            id: 2,
+            name: 'Cap',
+            description: '',
+            category: 'Одежда',
+            price: 5,
+        },
+        {
+            id: 3,
+            name: 'Pants',
+            description: '',
+            category: 'Одежда',
+            price: 5,
+        },
+        {
+            id: 4,
+            name: 'Диван',
+            description: '',
+            category: 'Для дома',
+            price: 20,
+        },
+    ];
+	
+    return {
+        ...original,
+        applyCategories: jest.fn().mockReturnValue(products),
+    };
+});
+
 describe('Main page test', () => {
-    jest.spyOn(Date.prototype, 'toLocaleTimeString').mockReturnValue(
+    jest.spyOn(Date.prototype, 'toLocaleTimeString').mockReturnValueOnce(
         '14:00:00'
     );
-	
+
     it('should render correctly', () => {
         const rendered = render(<MainPage />);
-
         expect(rendered.asFragment()).toMatchSnapshot();
+    });
+
+    it('should update catalog when category click', () => {
+        const rendered = render(<MainPage />);
+
+        expect(applyCategories).toHaveBeenCalledTimes(1);
+        fireEvent.click(
+            rendered.container.getElementsByClassName('categories__badge')[0]
+        );
+        expect(applyCategories).toHaveBeenCalledTimes(2);
     });
 });
