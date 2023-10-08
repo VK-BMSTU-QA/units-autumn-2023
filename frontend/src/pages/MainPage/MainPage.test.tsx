823139f31375847f51772b266b64f61c18382f6b
Shagalov Viacheslav <shagalovva@student.bmstu.ru>
2023-10-08 16:38:28 +0300

    Переделал тесты для MainPage.

--- frontend/src/pages/MainPage/MainPage.test.tsx
+++ frontend/src/pages/MainPage/MainPage.test.tsx
@@ -3,53 +3,42 @@ import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
 import { MainPage } from './MainPage';
-import { updateCategories } from '../../utils';
+import { applyCategories, updateCategories } from '../../utils';
+import { useCurrentTime, useProducts } from '../../hooks';
 
-jest.mock('../../utils/updateCategories', () => {
+jest.mock('../../hooks', () => {
+    const originalModule = jest.requireActual('../../hooks');
+
     return {
         __esModule: true,
+        ...originalModule,
+        useCurrentTime: jest.fn(() => '13:58:00'),
+    };
+});
+
+jest.mock('../../utils', () => {
+    const originalModule = jest.requireActual('../../utils');
+
+    return {
+        __esModule: true,
+        ...originalModule,
+        applyCategories: jest.fn(() => useProducts()),
         updateCategories: jest.fn(() => ['Одежда']),
     };
 });
 
 afterEach(jest.clearAllMocks);
 describe('MainPage test', () => {
-    const date = new Date('2019-05-14T11:01:58.135Z');
-    jest
-        .spyOn(global, 'Date')
-        .mockImplementation(() => date);
-
     it('should render correctly', () => {
-
         const rendered = render(<MainPage />);
 
         expect(rendered.asFragment()).toMatchSnapshot();
     });
 
-    it('should add title', () => {
+    it('should call current time function', () => {
         const rendered = render(<MainPage />);
 
-        expect(rendered.getByText('VK Маркет')).toHaveClass(
-            'main-page__title'
-        );
-    });
-
-    it('should add element for time', () => {
-        const rendered = render(<MainPage />);
-
-        expect(rendered.getByText('14:01:58')).toBeInTheDocument();
-    });
-
-    it('should add categories', () => {
-        const rendered = render(<MainPage />);
-
-        expect(rendered.container.getElementsByClassName('categories').length).toBe(1);
-    });
-
-    it('should add products', () => {
-        const rendered = render(<MainPage />);
-
-        expect(rendered.container.getElementsByClassName('product-card').length).toBe(4);
+        expect(useCurrentTime).toHaveBeenCalledTimes(1);
     });
 
     it('should call update categories when category click', () => {
@@ -58,5 +47,13 @@ describe('MainPage test', () => {
         expect(updateCategories).toHaveBeenCalledTimes(0);
         fireEvent.click(rendered.container.getElementsByClassName('categories__badge')[0]);
         expect(updateCategories).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call apply categories when category click', () => {
+        const rendered = render(<MainPage />);
+
+        expect(applyCategories).toHaveBeenCalledTimes(1);
+        fireEvent.click(rendered.container.getElementsByClassName('categories__badge')[0]);
+        expect(applyCategories).toHaveBeenCalledTimes(2);
     });
 });
