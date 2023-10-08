1e57d5bd85e32b3df5971ba27c6a7218090e8733
qqqq4u <danila16102003@rambler.ru>
2023-10-08 19:57:11 +0300

    tests for front

---
+++ frontend/src/pages/MainPage/MainPage.test.tsx
@@ -0,0 +1,37 @@
+import { MainPage } from "./MainPage";
+import { fireEvent, render } from "@testing-library/react";
+import '@testing-library/jest-dom';
+import { useCurrentTime} from '../../hooks';
+import { updateCategories } from '../../utils';
+
+afterEach(jest.clearAllMocks);
+
+jest.mock('../../hooks/useCurrentTime', () => {
+    return {
+        useCurrentTime: jest.fn(() => '12:00:00'),
+    };
+});
+
+jest.mock('../../utils/updateCategories', () => {
+    return {
+        updateCategories: jest.fn(() => ['Для дома']),
+    };
+});
+
+describe('tests for MainPage', () => {
+    it('Render test(with time)', () => {
+        const MyPageRendered = render(<MainPage />);
+        expect(MyPageRendered.asFragment()).toMatchSnapshot();
+        expect(useCurrentTime).toHaveBeenCalledTimes(1);
+    });
+    it('Apply category call check', () => {
+        const MyPageRendered = render(<MainPage />);
+        expect(updateCategories).toHaveBeenCalledTimes(0);
+        fireEvent.click(
+            MyPageRendered.baseElement.getElementsByClassName(
+                'categories__badge'
+            )[1]
+        );
+        expect(updateCategories).toHaveBeenCalledTimes(1);
+    });
+});
