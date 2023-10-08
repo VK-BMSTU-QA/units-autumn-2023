27eccab75e28783055757e0511c6244f1258b7e3
nikstarling <rioskill@yandex.ru>
2023-10-08 14:51:17 +0300

    added type tests for backend, updateCategories test for frontend

--- frontend/src/pages/MainPage/MainPage.test.tsx
+++ frontend/src/pages/MainPage/MainPage.test.tsx
@@ -3,11 +3,13 @@ import '@testing-library/jest-dom';
 import { MainPage } from "./MainPage";
 import * as currentTimeHook from "../../hooks/useCurrentTime"
 import * as applyCategoriesUtil from "../../utils/applyCategories"
+import * as updateCategoriesUtil from "../../utils/updateCategories"
 
 const mockCurrentTime = jest.spyOn(currentTimeHook, 'useCurrentTime');
 mockCurrentTime.mockReturnValue('16:10:15')
 
 const mockApplyCategories = jest.spyOn(applyCategoriesUtil, 'applyCategories');
+const mockUpdateCategories = jest.spyOn(updateCategoriesUtil, 'updateCategories')
 
 const findNodeWithText = (nodes: HTMLCollection, text: string) => {
     for (const node of nodes) {
@@ -26,7 +28,7 @@ describe('Main page test', () => {
         expect(rendered.asFragment()).toMatchSnapshot();
     });
 
-    it('should call updateCategories on category click', () => {
+    it('should call applyCategories on category click', () => {
         const {container} = render(<MainPage/>);
 
         const categoryButtons = container.getElementsByClassName('categories__badge');
@@ -37,5 +39,18 @@ describe('Main page test', () => {
         expect(mockApplyCategories).toHaveBeenCalledTimes(1);
         fireEvent.click(clothButton!);
         expect(mockApplyCategories).toHaveBeenCalledTimes(2);
+    });
+
+    it('should call updateCategories on category click', () => {
+        const {container} = render(<MainPage/>);
+
+        const categoryButtons = container.getElementsByClassName('categories__badge');
+        const clothButton = findNodeWithText(categoryButtons, 'Одежда');
+
+        expect(clothButton).not.toBeUndefined;
+
+        expect(mockUpdateCategories).toHaveBeenCalledTimes(0);
+        fireEvent.click(clothButton!);
+        expect(mockUpdateCategories).toHaveBeenCalledTimes(1);
     });
 });
