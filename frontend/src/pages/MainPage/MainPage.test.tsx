460a8b466ae0b8b9077400f4afaac876c4369ac2
Gev0rg <gevorkgabrielan@gmail.com>
2023-10-08 12:20:33 +0300

    test: add test for frontend/MainPage with coverage 100

---
+++ frontend/src/pages/MainPage/MainPage.test.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { MainPage } from './MainPage';
+import { fireEvent, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { useCurrentTime } from '../../hooks/useCurrentTime';
+
+afterEach(jest.clearAllMocks);
+
+jest.mock('../../hooks/useCurrentTime', () => {
+    return {
+        __esModule: true,
+        useCurrentTime: jest.fn(() => '11:11:11'),
+    };
+});
+
+describe('test MainPage', () => {
+    it('correct render', () => {
+        const rendered = render(<MainPage />);
+
+        expect(rendered.asFragment()).toMatchSnapshot();
+
+        expect(useCurrentTime).toHaveBeenCalledTimes(1);
+    });
+
+    it('update categories on click', () => {
+        const rendered = render(<MainPage />);
+
+        const time = rendered.baseElement.getElementsByTagName('h3')[0];
+        expect(time.textContent).toEqual('11:11:11');
+
+        const products =
+            rendered.baseElement.getElementsByClassName('product-card');
+        expect(products.length).toEqual(4);
+
+        const clothesButton =
+            rendered.baseElement.getElementsByClassName('categories__badge')[0];
+        fireEvent.click(clothesButton);
+
+        expect(products.length).toEqual(1);
+    });
+});
