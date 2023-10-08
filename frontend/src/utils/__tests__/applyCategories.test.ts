5fcc1472f8591b887ff9dbd33b1388f1ad0586bb
Shagalov Viacheslav <shagalovva@student.bmstu.ru>
2023-10-08 17:01:20 +0300

    Переделал тесты для applyCategories.

--- frontend/src/utils/__tests__/applyCategories.test.ts
+++ frontend/src/utils/__tests__/applyCategories.test.ts
@@ -96,41 +96,7 @@ describe('test apply category function', () => {
     });
 
     it('should not apply categories', () => {
-        let categories: Category[] = [];
-        let result: Product[] = [
-            {
-                id: 1,
-                name: 'IPhone 14 Pro',
-                description: 'Latest iphone, buy it now',
-                price: 999,
-                priceSymbol: '$',
-                category: 'Электроника',
-                imgUrl: '/iphone.png',
-            },
-            {
-                id: 2,
-                name: 'Костюм гуся',
-                description: 'Запускаем гуся, работяги',
-                price: 1000,
-                priceSymbol: '₽',
-                category: 'Одежда',
-            },
-            {
-                id: 3,
-                name: 'Настольная лампа',
-                description: 'Говорят, что ее использовали в pixar',
-                price: 699,
-                category: 'Для дома',
-                imgUrl: '/lamp.png',
-            },
-            {
-                id: 4,
-                name: 'Принтер',
-                description: 'Незаменимая вещь для студента',
-                price: 7000,
-                category: 'Электроника',
-            },
-        ];
-        expect(applyCategories(products, categories)).toStrictEqual(result);
+        let result = [...products];
+        expect(applyCategories(products, [])).toStrictEqual(result);
     });
 });
