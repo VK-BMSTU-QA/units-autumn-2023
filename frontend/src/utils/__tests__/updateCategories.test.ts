2f581d903094d64f6ae0c7777d980b45839e1cc2
CyTb-On-Fire <cytb2024@gmail.com>
2023-10-08 23:00:36 +0300

    pr fix

--- frontend/src/utils/__tests__/updateCategories.test.ts
+++ frontend/src/utils/__tests__/updateCategories.test.ts
@@ -2,12 +2,19 @@ import { updateCategories } from "../updateCategories"
 import { Category } from "../../types"
 
 describe('test update categories function', () => {
-    it('should return categories without changed if it in categories, or including this category', () => {
+    it('should return categories without changed', () => {
         const cat1: Category = 'Для дома'
         const cat2: Category = 'Одежда'
         const cat3: Category = 'Электроника'
         
         expect(updateCategories([cat1, cat2, cat3], cat1)).toStrictEqual([cat2, cat3])
+    })
+
+    it('should return categories with changed', () => {
+        const cat1: Category = 'Для дома'
+        const cat2: Category = 'Одежда'
+        const cat3: Category = 'Электроника'
+
         expect(updateCategories([cat1, cat2], cat3)).toStrictEqual([cat1, cat2, cat3])
     })
 })
