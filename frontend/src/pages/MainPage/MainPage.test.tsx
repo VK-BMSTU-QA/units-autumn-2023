8ce3cd51738d6918147f025d7179adea6384cbdd
DESOLATE17 <taktashovadasha@yandex.ru>
2023-10-24 00:04:48 +0300

    fixed main page test

--- frontend/src/pages/MainPage/MainPage.test.tsx
+++ frontend/src/pages/MainPage/MainPage.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render, fireEvent} from '@testing-library/react';
+import {render, fireEvent, getAllByTestId} from '@testing-library/react';
 import * as currentTimeHook from "../../hooks/useCurrentTime"
 import '@testing-library/jest-dom';
 import {MainPage} from './MainPage';
@@ -18,14 +18,22 @@ describe('MainPage test', () => {
 
     it('should call applyCategories on category click', () => {
         const mockApplyCategories = jest.spyOn(applyCategoriesUtil, 'applyCategories');
-        const {getByText} = render(<MainPage/>);
+        const container = render(<MainPage/>);
 
-        const categoryElement = getByText((content, element) => {
-            return element?.tagName.toLowerCase() === 'div' && element.classList.contains('categories__badge') && content === 'Одежда';
-        });
+        const categoryElementsBefore = Array.from(document.querySelectorAll('.categories__badge'));
+        const categoryElementBefore = categoryElementsBefore.find((element) => element.textContent === 'Одежда');
 
         expect(mockApplyCategories).toHaveBeenCalledTimes(1);
-        fireEvent.click(categoryElement);
-        expect(mockApplyCategories).toHaveBeenCalledTimes(2);
+        expect(categoryElementBefore).toBeTruthy();
+
+        if (categoryElementBefore) {
+            fireEvent.click(categoryElementBefore);
+
+            const categoryElementsAfter = Array.from(document.querySelectorAll('.categories__badge'));
+            const categoryElementAfter = categoryElementsAfter.find((element) => element.textContent === 'Одежда');
+
+            expect(mockApplyCategories).toHaveBeenCalledTimes(2);
+            expect(categoryElementsAfter.length).toEqual(categoryElementsBefore.length)
+        }
     });
 });
