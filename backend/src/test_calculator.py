27b0b68897db22eb54b84e852816bf21096943ba
DESOLATE17 <taktashovadasha@yandex.ru>
2023-10-24 20:39:58 +0300

    fixed frontend test and python tests

--- backend/src/test_calculator.py
+++ backend/src/test_calculator.py
@@ -8,50 +8,39 @@ class TestCalculator(unittest.TestCase):
     def setUp(self):
         self.calculator = Calculator()
 
-    def test_add(self):
+    def test_add_basic(self):
         self.assertEqual(self.calculator.addition(1, 2), 3)
 
     def test_add_negative(self):
         self.assertEqual(self.calculator.addition(-1, -2), -3)
-
-    def test_add_negative_and_positive(self):
         self.assertEqual(self.calculator.addition(-7, 3), -4)
 
     def test_add_float(self):
         self.assertAlmostEqual(self.calculator.addition(0.5, 0.45), 0.95)
-
-    def test_add_float_negative(self):
         self.assertAlmostEqual(self.calculator.addition(-0.5, -0.45), -0.95)
 
     def test_add_str(self):
         self.assertEqual(self.calculator.addition('ab', 'bc'), 'abbc')
-
-    def test_add_str_and_nub(self):
         self.assertRaises(TypeError, self.calculator.addition, 'ab', 5)
-
-    def test_add_None(self):
-        self.assertRaises(TypeError, self.calculator.addition, None, None)
 
     def test_add_arrays(self):
         self.assertEqual(self.calculator.addition([1, 2], [3, 4]), [1, 2, 3, 4])
 
-    def test_add_nan(self):
+    def test_add_wrong_types(self):
+        self.assertRaises(TypeError, self.calculator.addition, None, None)
         self.assertTrue(math.isnan(self.calculator.addition(1, math.nan)))
-
-    def test_add_inf(self):
         self.assertEqual(self.calculator.addition(1, math.inf), math.inf)
 
     def test_add_complex(self):
         self.assertEqual(self.calculator.addition(1, 1j), 1+1j)
 
 
-    def test_mul(self):
+
+    def test_mul_basic(self):
         self.assertEqual(self.calculator.multiplication(2, 3), 6)
 
-    def test_mul_negative_and_positive(self):
+    def test_mul_negative(self):
         self.assertEqual(self.calculator.multiplication(2, -4), -8)
-
-    def test_mul_double_negative(self):
         self.assertEqual(self.calculator.multiplication(-2, -3), 6)
 
     def test_mul_float(self):
@@ -63,33 +52,25 @@ class TestCalculator(unittest.TestCase):
     def test_mul_inf(self):
         self.assertEqual(self.calculator.multiplication(math.inf, 1), math.inf)
 
-    def test_mul_nan(self):
-        self.assertTrue(math.isnan(self.calculator.multiplication(1, math.nan)))
-
     def test_mul_str(self):
         self.assertEqual(self.calculator.multiplication(5, 'a'), 'aaaaa')
 
-    def test_mul_None(self):
-        self.assertRaises(TypeError, self.calculator.multiplication, 1, None)
-
     def test_mul_wrong_type(self):
         self.assertRaises(TypeError, self.calculator.multiplication, 'aaaa', [1,2,3])
+        self.assertRaises(TypeError, self.calculator.multiplication, 1, None)
+        self.assertTrue(math.isnan(self.calculator.multiplication(1, math.nan)))
 
 
 
-    def test_sub(self):
+    def test_sub_basic(self):
         self.assertEqual(self.calculator.subtraction(3, 2), 1)
 
     def test_sub_negative(self):
         self.assertEqual(self.calculator.subtraction(4, -3), 7)
-
-    def test_sub_greater_num_from_less_num(self):
         self.assertEqual(self.calculator.subtraction(4, 5), -1)
 
     def test_sub_float(self):
         self.assertAlmostEqual(self.calculator.subtraction(0.5, 0.25), 0.25)
-
-    def test_sub_float_negative(self):
         self.assertAlmostEqual(self.calculator.subtraction(0.1, -0.25), 0.35)
 
     def test_sub_str(self):
@@ -97,6 +78,7 @@ class TestCalculator(unittest.TestCase):
 
     def test_sub_None(self):
         self.assertRaises(TypeError, self.calculator.subtraction, None, 1)
+
 
 
     def test_div(self):
@@ -107,8 +89,6 @@ class TestCalculator(unittest.TestCase):
 
     def test_div_float(self):
         self.assertAlmostEqual(self.calculator.division(7, 2), 3.5)
-
-    def test_div_float_negative(self):
         self.assertAlmostEqual(self.calculator.division(7, -2), -3.5)
 
     def test_div_by_zero(self):
@@ -122,7 +102,6 @@ class TestCalculator(unittest.TestCase):
 
     def test_div_periodic(self):
         self.assertAlmostEqual(self.calculator.division(1, 3), 0.3333333333333333)
-
 
 
     def test_abs_positive(self):
@@ -145,43 +124,27 @@ class TestCalculator(unittest.TestCase):
     def test_degree_positive(self):
         self.assertAlmostEqual(self.calculator.degree(2, 4), 16.0)
 
-    def test_degree_negative_degree(self):
+    def test_degree_negative(self):
         self.assertAlmostEqual(self.calculator.degree(2, -2), 0.25)
-
-    def test_degree_negative_base_odd(self):
         self.assertAlmostEqual(self.calculator.degree(-2, 3), -8.0)
-
-    def test_degree_negative_base_even(self):
         self.assertAlmostEqual(self.calculator.degree(-2, 4), 16.0)
-
-    def test_degree_double_negative_odd(self):
         self.assertAlmostEqual(self.calculator.degree(-2, -3), -0.125)
-
-    def test_degree_double_negative_even(self):
         self.assertAlmostEqual(self.calculator.degree(-2, -4), 0.0625)
 
-    def test_degree_float_base(self):
+    def test_degree_float(self):
         self.assertAlmostEqual(self.calculator.degree(0.5, 3), 0.125)
-
-    def test_degree_float_degree(self):
         self.assertAlmostEqual(self.calculator.degree(16, 0.5), 4.0)
 
     def test_degree_one(self):
         self.assertAlmostEqual(self.calculator.degree(1, 100), 1)
-
-    def test_degree_one_float_degree(self):
-            self.assertAlmostEqual(self.calculator.degree(1, 0.5), 1)
+        self.assertAlmostEqual(self.calculator.degree(1, 0.5), 1)
 
     def test_degree_str(self):
         self.assertRaises(TypeError, self.calculator.degree, 'a', 2)
 
-    def test_degree_None_degree(self):
+    def test_degree_wrong_type_degree(self):
         self.assertRaises(TypeError, self.calculator.degree, 2, None)
-
-    def test_degree_inf_degree(self):
         self.assertEqual(self.calculator.degree(4, math.inf), math.inf)
-
-    def test_degree_inf_base(self):
         self.assertEqual(self.calculator.degree(math.inf, 2), math.inf)
 
 
@@ -192,13 +155,9 @@ class TestCalculator(unittest.TestCase):
     def test_ln_e(self):
         self.assertAlmostEqual(self.calculator.ln(math.e), 1)
 
-    def test_ln_str(self):
+    def test_ln_wrong_type(self):
         self.assertRaises(TypeError, self.calculator.ln, 'a')
-
-    def test_ln_None(self):
         self.assertRaises(TypeError, self.calculator.ln, None)
-
-    def test_ln_inf(self):
         self.assertEqual(self.calculator.ln(math.inf), math.inf)
 
 
@@ -208,31 +167,19 @@ class TestCalculator(unittest.TestCase):
     def test_log_float(self):
         self.assertAlmostEqual(self.calculator.log(16, 0.5), -4.0)
 
-    def test_log_negative_base(self):
+    def test_log_negative(self):
         self.assertRaises(ValueError, self.calculator.log, 16, -2)
-
-    def test_log_negative_param(self):
         self.assertRaises(ValueError, self.calculator.log, -16, 2)
 
     def test_log_base_1(self):
         self.assertRaises(ZeroDivisionError, self.calculator.log, 5, 1)
 
-    def test_log_str_param(self):
+    def test_log_wromg_type(self):
         self.assertRaises(TypeError, self.calculator.log, 'a', 2)
-
-    def test_log_str_base(self):
         self.assertRaises(TypeError, self.calculator.log, 16, 'a')
-
-    def test_log_None_param(self):
         self.assertRaises(TypeError, self.calculator.log, None, 2)
-
-    def test_log_inf_base(self):
         self.assertEqual(self.calculator.log(16, math.inf), 0.0)
-
-    def test_log_inf_param(self):
         self.assertEqual(self.calculator.log(math.inf, 4), math.inf)
-
-    def test_log_None_base(self):
         self.assertRaises(TypeError, self.calculator.log, 16, None)
 
 
@@ -246,16 +193,10 @@ class TestCalculator(unittest.TestCase):
     def test_sqrt_imaginary(self):
         self.assertAlmostEqual(self.calculator.sqrt(-1), 1j)
 
-    def test_sqrt_str(self):
+    def test_sqrt_wrong_type(self):
         self.assertRaises(TypeError, self.calculator.sqrt, 'a')
-
-    def test_sqrt_None(self):
         self.assertRaises(TypeError, self.calculator.sqrt, None)
-
-    def test_sqrt_inf(self):
         self.assertEqual(self.calculator.sqrt(math.inf), math.inf)
-
-    def test_sqrt_nan(self):
         self.assertTrue(math.isnan(self.calculator.sqrt(math.nan)))
 
 
@@ -266,31 +207,19 @@ class TestCalculator(unittest.TestCase):
     def test_nth_root_float(self):
         self.assertAlmostEqual(self.calculator.nth_root(10, 3), 2.154434690031884)
 
-    def test_nth_root_negative_root(self):
-        self.assertAlmostEqual(self.calculator.nth_root(8, -3), 0.5)
-
     def test_nth_root_negative(self):
+        self.assertAlmostEqual(self.calculator.nth_root(8, -3), 0.5)
         self.assertAlmostEqual(self.calculator.nth_root(-1, 3), 0.5000000000000001+0.8660254037844386j)
 
     def test_nth_root_imaginary(self):
         self.assertAlmostEqual(self.calculator.nth_root(-16, 4), complex(2 ** 0.5, 2 ** 0.5))
 
-    def test_nth_root_str_base(self):
+    def test_nth_root_wrong_type(self):
         self.assertRaises(TypeError, self.calculator.nth_root, 'a', 2)
-
-    def test_nth_root_str_root(self):
         self.assertRaises(TypeError, self.calculator.nth_root, 2, 'a')
-
-    def test_nth_root_None_base(self):
         self.assertRaises(TypeError, self.calculator.nth_root, None, 2)
-
-    def test_nth_root_None_root(self):
         self.assertRaises(TypeError, self.calculator.nth_root, 2, None)
-
-    def test_nth_root_inf_root(self):
         self.assertEqual(self.calculator.nth_root(4, math.inf), 1.0)
-
-    def test_nth_root_inf_base(self):
         self.assertEqual(self.calculator.nth_root(math.inf, 10), math.inf)
 
 if __name__ == "__main__":
--- frontend/src/utils/__tests__/applyCategories.ts
+++ frontend/src/utils/__tests__/applyCategories.ts
@@ -66,21 +66,21 @@ describe('test apply categories function', () => {
             price: 7000,
             category: 'Электроника',
         }]);
-        expect(applyCategories(products, ['Одежда', 'Для дома'])).toStrictEqual([{
-            id: 1,
-            name: 'IPhone 14 Pro',
-            description: 'Latest iphone, buy it now',
-            price: 999,
-            priceSymbol: '$',
-            category: 'Электроника',
-            imgUrl: '/iphone.png',
-        }, {
-            id: 2,
-            name: 'Костюм гуся',
-            description: 'Запускаем гуся, работяги',
-            price: 1000,
-            priceSymbol: '₽',
-            category: 'Одежда',
-        }]);
+        expect(applyCategories(products, ['Одежда', 'Для дома'])).toStrictEqual([
+            {
+                id: 2,
+                name: 'Костюм гуся',
+                description: 'Запускаем гуся, работяги',
+                price: 1000,
+                priceSymbol: '₽',
+                category: 'Одежда',
+            }, {
+                id: 3,
+                name: 'Настольная лампа',
+                description: 'Говорят, что ее использовали в pixar',
+                price: 699,
+                category: 'Для дома',
+                imgUrl: '/lamp.png',
+            }]);
     });
 });
