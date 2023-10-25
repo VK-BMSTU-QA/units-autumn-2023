d722cc078d198221f9fe72f4ca1bf1e80a1e65df
CyTb-On-Fire <cytb2024@gmail.com>
2023-10-25 23:41:15 +0000

    mock fix

--- frontend/src/pages/MainPage/__tests__/MainPage.test.tsx
+++ frontend/src/pages/MainPage/__tests__/MainPage.test.tsx
@@ -29,7 +29,7 @@ describe('main page test', () => {
         expect(mockUpdateCategories).toBeCalledTimes(0);
         expect(mockApplyCategories).toBeCalledTimes(1);
 
-        expect(rendered.container.getElementsByClassName('product-card')).toHaveLength(2)
+        expect(rendered.container.getElementsByClassName('product-card')).toHaveLength(4)
 
         fireEvent.click(rendered.baseElement.getElementsByClassName('categories__badge')[0]);
         expect(mockUpdateCategories).toBeCalledTimes(1);
