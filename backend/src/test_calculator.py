c94763737d74e50f4fecd3ea4553d29be4c321c3
Alik Nigmatullin <NigAlik020503@yandex.ru>
2023-10-26 00:13:07 +0300

    finished fixes tests for backend

--- backend/src/test_calculator.py
+++ backend/src/test_calculator.py
@@ -1,7 +1,6 @@
 import unittest
 import math
-from calculator import Calculator
-# from src.calculator import Calculator
+from src.calculator import Calculator
 
 class TestCalculator(unittest.TestCase):
 
@@ -17,6 +16,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calculator.addition([1, 2, 3], [4, 5]), [1, 2, 3, 4, 5])
         self.assertEqual(self.calculator.addition((1, 2), (3, 4, 5)), (1, 2, 3, 4, 5))
         self.assertEqual(self.calculator.addition(math.inf, math.inf), math.inf)
+
         self.assertRaises(TypeError, self.calculator.addition, None, None)
         self.assertRaises(TypeError, self.calculator.addition, "abc", 1)
         self.assertRaises(TypeError, self.calculator.addition, "abc", ["a", "b", "c"])
@@ -31,6 +31,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calculator.multiplication([1, 2], 3), [1, 2, 1, 2, 1, 2])
         self.assertEqual(self.calculator.multiplication((1, 2), 3), (1, 2, 1, 2, 1, 2))
         self.assertEqual(self.calculator.multiplication(math.inf, math.inf), math.inf)
+
         self.assertRaises(TypeError, self.calculator.multiplication, "abc", [1, 2])
         self.assertRaises(TypeError, self.calculator.multiplication, [1, 2], (1, 2))
         self.assertRaises(TypeError, self.calculator.multiplication, [1, 2], [1, 2])
@@ -42,6 +43,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calculator.subtraction(-1, -2), 1)
         self.assertAlmostEqual(self.calculator.subtraction(1.1, 2.2), -1.1)
         self.assertEqual(self.calculator.subtraction(math.inf, 100), math.inf)
+
         self.assertRaises(TypeError, self.calculator.subtraction, None, None)
         self.assertRaises(TypeError, self.calculator.subtraction, "abc", 1)
         self.assertRaises(TypeError, self.calculator.subtraction, "abc", ["a", "b", "c"])
@@ -56,6 +58,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calculator.division(-4, 2), -2)
         self.assertAlmostEqual(self.calculator.division(1.628, 2.2), 0.74)
         self.assertEqual(self.calculator.division(math.inf, 100), math.inf)
+
         self.assertRaises(TypeError, self.calculator.division, "abc", [1, 2])
         self.assertRaises(TypeError, self.calculator.division, [1, 2], (1, 2))
         self.assertRaises(TypeError, self.calculator.division, [1, 2], [1, 2])
@@ -71,6 +74,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calculator.adsolute(-1.2), 1.2)
         self.assertEqual(self.calculator.adsolute(math.inf), math.inf)
         self.assertEqual(self.calculator.adsolute(-math.inf), math.inf)
+
         self.assertRaises(TypeError, self.calculator.adsolute, "abc")
         self.assertRaises(TypeError, self.calculator.adsolute, [1, 2])
         self.assertRaises(TypeError, self.calculator.adsolute, (1, 2))
@@ -87,6 +91,7 @@ class TestCalculator(unittest.TestCase):
         self.assertAlmostEqual(self.calculator.degree(2, 2.2), 4.59479342)
         self.assertEqual(self.calculator.degree(math.inf, 2), math.inf)
         self.assertEqual(self.calculator.degree(2, math.inf), math.inf)
+
         self.assertRaises(TypeError, self.calculator.degree, "abc", 2)
         self.assertRaises(TypeError, self.calculator.degree, [1, 2], 2)
         self.assertRaises(TypeError, self.calculator.degree, (1, 2), 2)
@@ -97,6 +102,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calculator.ln(1), 0)
         self.assertAlmostEqual(self.calculator.ln(2.5), 0.916290732)
         self.assertEqual(self.calculator.ln(math.inf), math.inf)
+
         self.assertRaises(TypeError, self.calculator.degree, None)
         self.assertRaises(TypeError, self.calculator.degree, -2.5)
         self.assertRaises(TypeError, self.calculator.degree, "abc")
@@ -106,14 +112,51 @@ class TestCalculator(unittest.TestCase):
     def test_log(self):
         self.assertEqual(self.calculator.log(2, 2), 1)
         self.assertEqual(self.calculator.log(8, 2), 3)
-    
+        self.assertAlmostEqual(self.calculator.log(2, 10), 0.301029996)
+        self.assertAlmostEqual(self.calculator.log(2.2, 10), 0.342422681)
+        self.assertAlmostEqual(self.calculator.log(10, 2.2), 2.92036730043365)
+        self.assertAlmostEqual(self.calculator.log(6.8, 2.2), 2.43123180598686)
+        self.assertEqual(self.calculator.log(math.inf, 2.2), math.inf)
+        self.assertEqual(self.calculator.log(6.8, math.inf), 0.0)
+
+        self.assertRaises(TypeError, self.calculator.log, None, None)
+        self.assertRaises(ValueError, self.calculator.log, -2.5, 5)
+        self.assertRaises(TypeError, self.calculator.log, [1, 2], 5)
+        self.assertRaises(TypeError, self.calculator.log, (1, 2), 5)
+        self.assertRaises(TypeError, self.calculator.log, "abc", 5)
+
     def test_sqrt(self):
         self.assertEqual(self.calculator.sqrt(16), 4)
         self.assertEqual(self.calculator.sqrt(0), 0)
+        self.assertAlmostEqual(self.calculator.sqrt(-16), 2.4492935982947064e-16+4j)
+        self.assertAlmostEqual(self.calculator.sqrt(7.5), 2.73861279)
+        self.assertEqual(self.calculator.sqrt(math.inf), math.inf)
+        self.assertEqual(self.calculator.sqrt(-math.inf), math.inf)
+
+        self.assertRaises(TypeError, self.calculator.sqrt, None)
+        self.assertRaises(TypeError, self.calculator.sqrt, "abc")
+        self.assertRaises(TypeError, self.calculator.sqrt, [1, 2])
+        self.assertRaises(TypeError, self.calculator.sqrt, (1, 2))
 
     def test_nth_root(self):
         self.assertEqual(self.calculator.nth_root(6, 1), 6)
         self.assertEqual(self.calculator.nth_root(4, 2), 2)
+        self.assertEqual(self.calculator.nth_root(0, 2), 0.0)
+        self.assertAlmostEqual(self.calculator.nth_root(-4, 2), 1.2246467991473532e-16+2j)
+        self.assertEqual(self.calculator.nth_root(4, -2), 0.5)
+        self.assertAlmostEqual(self.calculator.nth_root(7.5, 2), 2.7386127875258306)
+        self.assertAlmostEqual(self.calculator.nth_root(2, 7.5), 1.096824979694626)
+        self.assertEqual(self.calculator.nth_root(math.inf, 2), math.inf)
+        self.assertEqual(self.calculator.nth_root(4, math.inf), 1.0)
+
+        self.assertRaises(TypeError, self.calculator.nth_root, None, None)
+        self.assertRaises(TypeError, self.calculator.nth_root, "abc", 2)
+        self.assertRaises(TypeError, self.calculator.nth_root, [1, 2], 2)
+        self.assertRaises(TypeError, self.calculator.nth_root, (1, 2), 2)
+        self.assertRaises(TypeError, self.calculator.nth_root, 2, "abc")
+        self.assertRaises(TypeError, self.calculator.nth_root, 2, [1, 2])
+        self.assertRaises(TypeError, self.calculator.nth_root, 2, (1, 2))
+        self.assertRaises(ZeroDivisionError, self.calculator.nth_root, 4, 0)
 
 if __name__ == "__main__":
     unittest.main()
