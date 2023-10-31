34748c9da79373736f5a95c61d255e08e06cc84a
IvanStukalov <ivanstukalov2003@gmail.com>
2023-10-31 12:53:38 +0300

    reformat file update categories

--- frontend/src/utils/updateCategories.ts
+++ frontend/src/utils/updateCategories.ts
@@ -1,14 +1,14 @@
 import type {Category} from '../types';
 
 export const updateCategories = (
-  currentCategories: Category[],
-  changedCategories: Category
+    currentCategories: Category[],
+    changedCategories: Category
 ) => {
-  if (currentCategories.includes(changedCategories)) {
-    return currentCategories.filter(
-      (filter) => filter !== changedCategories
-    );
-  }
+    if (currentCategories.includes(changedCategories)) {
+        return currentCategories.filter(
+            (filter) => filter !== changedCategories
+        );
+    }
 
-  return [...currentCategories, changedCategories];
+    return [...currentCategories, changedCategories];
 };
